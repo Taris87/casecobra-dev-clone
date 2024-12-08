e69885850459aba7b10e6422d0a7c73af705887f
Steven Cleveland <steven.cleveland41@.yahoo.com>
2024-12-08 11:25:44 +0100

    Some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,7 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto flex flex-col items-center text-center lg:items-start lg:text-left">
               <div className="absolute -top-20 left-0 hidden w-28 lg:block">
-                {/* i forgot this div right here in the video, it's purely visual gradient and looks nice */}
-                <div className="absolute inset-x-0 bottom-0 h-28 bg-gradient-to-t from-slate-50 via-slate-50/50" />
+                <div className="absolute inset-x-0 bottom-0 h-28 bg-gradient-to-t from-slate-50 via-slate-50/10" />
                 <img src="/snake-1.png" className="w-full" />
               </div>
               <h1 className="relative mt-16 w-fit text-balance text-5xl font-bold !leading-tight tracking-tight text-gray-900 md:text-6xl lg:text-7xl">
